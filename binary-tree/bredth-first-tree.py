eed936df8b0baf9f831d2ae3fd5fa0c6573f00f1
Vikas <vikask99588@gmail.com>
2025-03-20 14:50:42 +0530

    Fixed typos

--- binary-tree/bredth-first-tree.py
+++ binary-tree/bredth-first-tree.py
@@ -2,18 +2,23 @@ from base_tree import BinaryTree, TreeNode
 from typing import Self
 
 
-class BredthFirstTree(BinaryTree):
+class BreathFirstTree(BinaryTree):
     def __init__(self, root: TreeNode):
         super().__init__(root)
 
-    def pre_order_traversal(self) -> Self:
+    def pre_order_iterative(self) -> Self:
         return self
-        ...
+
+    def post_order_iterative(self): ...
+
+    def in_order_iterative(self): ...
+
+    def level_order_iterative(self): ...
 
 
 def main():
     root = TreeNode(5)
-    bft = BredthFirstTree(root)
+    bft = BreathFirstTree(root)
     bft.pre_order_traversal().pp_tree()
 
 
